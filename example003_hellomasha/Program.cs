f3cf54b1f584a69bd138a3b3e8069127b120b46c
alubina92 <alubina25@gmail.com>
2023-02-10 13:06:52 +0300

    Created folder and program Hellomasha

---
+++ example003_hellomasha/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя:");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
