214c791c7638f52f6d2dd9a6a85eeff7e0d0babc
alubina92 <alubina25@gmail.com>
2023-02-10 14:22:50 +0300

    Пока с домашкой закончила

---
+++ example005_dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int distance = 10000;
+int ffs = 1;
+int sfs = 2;
+int ds = 5;
+int friends = 2;
+int time = 0;
+while(distance > 10)
+{
+    if(friends == 1)
+    {
+        time = distance / (ffs + ds);
+        friends = 2;
+    }
+    else
+    {
+        time = distance / (sfs + ds);
+        friends = 1;
+    }
+    distance = distance - (ffs + sfs)*time;
+    count = count + 1;
+}
+
+Console.WriteLine(count);
+
