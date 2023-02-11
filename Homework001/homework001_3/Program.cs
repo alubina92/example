b7467964c827f794af6d701910b5839a3a1809e9
alubina92 <alubina25@gmail.com>
2023-02-11 20:47:43 +0300

    Решила задачу 3

---
+++ Homework001/homework001_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+
+Console.WriteLine("Введите число: ");
+
+double a = Double.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число НЕ четное");
+}
