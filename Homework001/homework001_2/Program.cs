49097873e271ba6c24411ec943b426b07e498a36
alubina92 <alubina25@gmail.com>
2023-02-11 19:08:19 +0300

    Решила 2 задачу

---
+++ Homework001/homework001_2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Программа, которая ищет наибольшее из трех чисел");
+Console.WriteLine("Введите числа по порядку, нажимая enter");
+
+double a = Double.Parse(Console.ReadLine());
+double b = Double.Parse(Console.ReadLine());
+double c = Double.Parse(Console.ReadLine());
+
+if(a>b)
+{
+    if(a>c)
+    {
+      Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if(b>c)
+    {
+      Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+
+
