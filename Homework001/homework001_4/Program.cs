73159ec5631a6c9f0ebf61864305401dd11c7560
alubina92 <alubina25@gmail.com>
2023-02-13 09:20:38 +0300

    Доделала домашку

---
+++ Homework001/homework001_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+double N = Double.Parse(Console.ReadLine());
+int count = 1;
+
+while(count<=N)
+{
+    if (count % 2 == 0)
+    {
+         Console.WriteLine(count);
+        
+    }
+ 
+ count += 1;     
+}
+
+Console.WriteLine("Операция завершена.");
+
+
