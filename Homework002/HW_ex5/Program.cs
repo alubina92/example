fd4a80b288646065ef4f22257ed25954074b1160
alubina92 <alubina25@gmail.com>
2023-02-16 14:27:37 +0300

    Сделала задачу по простому палиндромом

---
+++ Homework002/HW_ex5/Program.cs
@@ -0,0 +1,21 @@
+﻿// написать программу, которая определяет является ли пятизначное число палиндромом.
+//Для обращения числа в степени n достаточно использовать метод Math .Pow( x , y ), где x – число, возводимое в степени, y – число, задающее степень.
+
+
+bool palindromom(int number)
+{
+if(number/10000 == number % 10 && (number / 1000)%10 == (number/10)%10 )
+{
+    return true;
+    Console.WriteLine("Число палиндромом)");
+}
+else
+{
+    return false;
+    Console.WriteLine("Число НЕ палиндромом");
+}
+}
+int abcde = new Random().Next(10000, 99999);
+Console.WriteLine(abcde);
+
+Console.WriteLine(palindromom(abcde));
