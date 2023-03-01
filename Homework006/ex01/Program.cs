0c62e83ae4a91c0cf6478f20f0858b7cb5094826
alubina92 <alubina25@gmail.com>
2023-03-01 15:16:15 +0300

    домашка6

---
+++ Homework006/ex01/Program.cs
@@ -0,0 +1,39 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int GetNumber(string text)
+{
+    System.Console.WriteLine("Введите число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Count(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i<array.Length; i++)
+    {
+        if(array[i]>0) count++;
+
+    }
+    System.Console.WriteLine($"Количество больше нуля равно: {count}");
+    return count;
+}
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+void FillArray(int[] array)
+{
+  for(int i = 0; i<array.Length; i++) array[i] = GetNumber($"Введите {array[i]} число: ");
+}
+void PrintArray(int[]array)
+{   
+    System.Console.Write("[");
+    for(int i = 0; i<array.Length; i++)
+    {
+    System.Console.Write($"{array[i]}, " );
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] numbers = CreateArray(GetNumber("Введите число "));
+FillArray(numbers);
+PrintArray(numbers);
+int count = Count(numbers);
---
+++ Lection004/massivtable/Program.cs
@@ -0,0 +1,42 @@
+﻿//зададим двумерный массив
+string[,] table = new string[2, 5];
+// table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4]
+// table[1, 0] table[1, 1] table[2, 2] table[3, 3] table[4, 4]
+
+table[1, 2] = "слово";
+
+void PrintArray(string[,] array)
+{
+    for( int i = 0; i<2; i++)
+    {
+        for(int j = 0; j<5; j++)
+        {
+            System.Console.WriteLine($" - {array[i,j]} -");
+        }
+    }
+}
+void PrintTwoArray(int[,] array)
+{
+    for( int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void FillTwoArray(int[,] array)
+{
+    for(int i = 0; i<array.GetLength(0);i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+             array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+//PrintArray(table);
+int[,] tableTwo = new int[3,6];
+FillTwoArray(tableTwo);
+PrintTwoArray(tableTwo);
